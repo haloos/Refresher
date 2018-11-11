1cfac59ea324a5ccbea03632fe543d9741260c07
Jason Paredes <haloos@me.com>
2018-11-10 22:49:12 -0500

    Built in Constructors

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
-// Person constructor  
-function Person(name, dob) {
-  this.name = name; 
-  //this.age = age;
-  this.birthday = new Date(dob);
-  this.calculateAge = function(){
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
+// String
+ 
+const name1 = 'Jason';
+const name2 = new String('Jason');
+
+//name2.foo = 'bar';
+//console.log(name2);
+
+console.log(typeof name2);
+
+if(name2 === 'Jeff'){
+  console.log('YES');
+} else {
+  console.log('NO');
 }
 
-//const jason = new Person('Jason', 40);
-//const matthew = new Person('Matthew', 36);
+// Number
+const num1 = 5;
+const num2 = new Number();
 
-//console.log(matthew.age);
+// Boolean
+const bool1 = true; 
+const bool2 = new Boolean(true); 
 
-const jason = new Person('Jason', '3/4/1978');
-console.log(jason.calculateAge());
+// Function 
+const getSum1 = function(x, y){
+  return x + y;
+}
+
+const getSum2 = new Function('x','y', 'return 1 + 1');
+
+// Object 
+const john1 = {name: "John"};
+const john2 = new Object({name: "John"});
+console.log(john2);
+
+// Arrays
+const arr1 = [1,2,3,4,]; 
+const arr2 = new Array(1,2,3,4,); 
+
+console.log(arr1); 
+
+// Regular Expressions
+const re1 = /\w+/; 
+const re2 = new RegExp('\\w+'); 
+
+console.log(re2);
