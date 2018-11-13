87be0007f09224d7a18044a829f3c8762718b07b
Jason Paredes <haloos@me.com>
2018-11-12 21:15:13 -0500

    Sub Classes

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 class Person {
-  constructor(firstName, lastName, dob) {
+  constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.birthday = new Date(dob); 
   }
 
   greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
   }
-
-  calculateAge() {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
-
-  getsMarried(newLastName) {
-    this.lastName = newLastName;
-  }
-
-  static addNumbers(x, y) {
-    return x + y;
-  }
 }
 
-const mary = new Person('Mary', 'Williams', '11-13-1980');
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership) {
+    super(firstName, lastName);
 
-mary.getsMarried();
+    this.phone = phone;
+    this.membership = membership;
+  }
 
-console.log(Person.addNumbers(1,2)); 
+static getMembershipCost() {  
+  return 500;
+}
+
+}
+
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard'); 
+
+console.log(john.greeting());
+
+console.log(Customer.getMembershipCost());
