be4de6bb603ded3ecd3869c8ff8a11c83f0dbb0a
Jason Paredes <haloos@me.com>
2018-11-26 11:48:53 -0500

    Callback functions

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
+const posts = [
+  {title: 'Post One', body: 'This is post one'},
+  {title: 'Post Two', body: 'This is post two'},
+];
 
-  greeting() {
-    return `Hello there ${this.firstName} ${this.lastName}`;
-  }
+/*function createPost(post) {
+  setTimeout(function() {
+    posts.push(post);
+  }, 2000);
 }
 
-class Customer extends Person {
-  constructor(firstName, lastName, phone, membership) {
-    super(firstName, lastName);
 
-    this.phone = phone;
-    this.membership = membership;
-  }
-
-static getMembershipCost() {  
-  return 500;
+function getPosts() {
+ setTimeout(function (){
+  let output = '';
+  posts.forEach(function(post){
+    output += `<li>${post.title}</li>`;
+  });
+  document.body.innerHTML = output;
+ }, 1000);
 }
 
+createPost({title: 'Post Three', body: 'This is post three'});
+
+getPosts();*/
+
+function createPost(post, callback) {
+  setTimeout(function() {
+    posts.push(post);
+    callback();
+  }, 2000);
 }
 
-const john = new Customer('John', 'Doe', '555-555-5555', 'Standard'); 
 
-console.log(john.greeting());
+function getPosts() {
+ setTimeout(function (){
+  let output = '';
+  posts.forEach(function(post){
+    output += `<li>${post.title}</li>`;
+  });
+  document.body.innerHTML = output;
+ }, 1000);
+}
 
-console.log(Customer.getMembershipCost());
+createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
