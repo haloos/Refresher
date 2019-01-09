1c7459dd975d758ed354731a944e74dc26abc40f
Jason Paredes <haloos@me.com>
2019-01-08 20:47:21 -0500

    Character Sets & Quantifiers

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-let re;
-// Literal Characters
-re = /hello/;
 re = /hello/i;
 
 // MetaCharacters Symbols
 re = /^h/i;       // Must start with
 re = /d$/i;       // Must ends with
 re = /^hello$/i;  // Must begin and end with 
-re = /h.llo/i;  // Matches any ONE character
-re = /h*llo/i;  // Matches any character 0 or more times 
+re = /h.llo/i;    // Matches any ONE character
+re = /h*llo/i;    // Matches any character 0 or more times 
+re = /gre?a?y/i;  // Optional character
+re = /gre?a?y\?/i; // Escape character
+
+
+// Brackets [] - Character Sets
+re  = /gr[ae]y/i;   // Must be an a or e 
+re  = /[GF]ray/i;   
 
 // String to match
-const str = 'Hello World';
+const str = 'Gray';
 
 // Log Results
 const result = re.exec(str);
@@ -25,4 +29,4 @@ function reTest(re, str) {
   }
 }
 
-reTest(re, str);
+reTest(re, str); 
