2c48c9ea8ed408fa45ecd0fe3dff98a5e16559aa
Jason Paredes <haloos@me.com>
2019-01-09 20:24:08 -0500

    Shorthand Characters Classes

--- app.js
+++ app.js
@@ -11,11 +11,24 @@ re = /gre?a?y\?/i; // Escape character
 
 
 // Brackets [] - Character Sets
-re  = /gr[ae]y/i;   // Must be an a or e 
-re  = /[GF]ray/i;   
+re  = /gr[ae]y/i;     // Must be an a or e 
+re  = /[GF]ray/i;     // Must be a G or F
+re  = /[^GF]ray/i;    // Match anyting except a G or F
+re  = /[A-Z]ray/;     // Match any uppercase letter
+re  = /[a-z]ray/;     // Match any lowercase letter
+re  = /[A-Za-z]ray/;  // Match any letter
+re = /[0-9]ray/;      // Match any digit
+
+// Braces {} - Quantifiers
+re = /Hel{2}o/i;      // Must occur exactly {m} amount of times
+re = /Hel{2,4}o/i;      // Must occur exactly {m} amount of times
+re = /Hel{2,}o/i;      // Must occur at least {m}  times
+
+// Paretheses () - Grouping
+re = /^([0-9]x){3}$/
 
 // String to match
-const str = 'Gray';
+const str = '3x3x3x';
 
 // Log Results
 const result = re.exec(str);
